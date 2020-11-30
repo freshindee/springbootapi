66f3987dc19cadd2b731a245b98f6706bb2bcdba
Silverleap Technology <Silver@kajen.local>
2020-12-01 04:37:37 +0530

    First Commit

---
+++ src/main/java/com/fitscorp/j2eemobileapi/restservices/restservices/RestservicesApplication.java
@@ -0,0 +1,15 @@
+package com.fitscorp.j2eemobileapi.restservices.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicesApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/fitscorp/j2eemobileapi/restservices/restservices/RestservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitscorp.j2eemobileapi.restservices.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

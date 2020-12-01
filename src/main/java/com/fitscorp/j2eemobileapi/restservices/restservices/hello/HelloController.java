5f736eceff031898e044d7f4667f46a87631596e
Silverleap Technology <Silver@kajen.local>
2020-12-01 09:59:29 +0530

    Hello World Base

---
+++ src/main/java/com/fitscorp/j2eemobileapi/restservices/restservices/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.fitscorp.j2eemobileapi.restservices.restservices.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping(method=RequestMethod.GET,path="/hello")
+	public String HellowWorld() {
+		return "Hello Workd";
+	}
+
+}

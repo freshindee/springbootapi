f3ecf6c5fef5286b60e728a457606ce4c54828b3
Silver <Silver@localhost>
2020-12-01 05:55:04 +0530

    test cmment added

--- src/main/java/com/fitscorp/j2eemobileapi/restservices/restservices/RestservicesApplication.java
+++ src/main/java/com/fitscorp/j2eemobileapi/restservices/restservices/RestservicesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestservicesApplication {
-
+// test comment added
 	public static void main(String[] args) {
 		SpringApplication.run(RestservicesApplication.class, args);
 	}
